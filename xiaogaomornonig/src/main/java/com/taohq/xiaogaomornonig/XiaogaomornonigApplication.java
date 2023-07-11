8a48555666f96f916f62e9b684c111086bf97305
taohq <taohq1125@163.com>
2023-07-11 14:33:40 +0800

    初始版本

---
+++ xiaogaomornonig/src/main/java/com/taohq/xiaogaomornonig/XiaogaomornonigApplication.java
@@ -0,0 +1,13 @@
+package com.taohq.xiaogaomornonig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaogaomornonigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaogaomornonigApplication.class, args);
+    }
+
+}
---
+++ xiaogaomornonig/src/test/java/com/taohq/xiaogaomornonig/XiaogaomornonigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taohq.xiaogaomornonig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaogaomornonigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
